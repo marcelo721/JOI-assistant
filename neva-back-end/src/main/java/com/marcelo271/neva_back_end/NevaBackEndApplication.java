da29576c7c11894485e8990b6f24e62ea31e73bb
marcelo721 <marceloh.sousa@alu.ufc.br>
2026-08-17 11:04:38 -0300

    feat: initialize backend

---
+++ neva-back-end/src/main/java/com/marcelo271/neva_back_end/NevaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo271.neva_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NevaBackEndApplication.class, args);
+	}
+
+}
---
+++ neva-back-end/src/test/java/com/marcelo271/neva_back_end/NevaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo271.neva_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NevaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
